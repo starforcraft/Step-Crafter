c144f852b294a7eee8912615b85c5dd5261e53a4
Starforcraft <starforcraft@yahoo.com>
2026-09-06 14:42:33 +0200

    Fixed pressing Alt + Click not working inside Grid

--- common/src/main/java/com/ultramega/stepcrafter/common/mixin/MixinAbstractGridScreen.java
+++ common/src/main/java/com/ultramega/stepcrafter/common/mixin/MixinAbstractGridScreen.java
@@ -19,8 +19,6 @@ import com.refinedmods.refinedstorage.common.support.widget.TextMarquee;
 import java.util.List;
 import javax.annotation.Nullable;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -28,7 +26,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -86,24 +83,25 @@ public abstract class MixinAbstractGridScreen<T extends AbstractGridContainerMen
         }
     }
 
-    @ModifyExpressionValue(method = "mouseClicked", at = @At(value = "INVOKE",
-        target = "Lcom/refinedmods/refinedstorage/common/api/grid/view/GridResource;"
-            + "isAutocraftable(Lcom/refinedmods/refinedstorage/api/resource/repository/ResourceRepository;)Z"), remap = false)
-    public boolean mouseClicked(final boolean original) {
-        final ItemStack carriedStack = this.getMenu().getCarried();
-        final GridResource resource = this.getCurrentGridResource();
-        if (resource != null
-            && ((resource.canExtract(carriedStack, this.getMenu().getRepository()) && !Screen.hasControlDown()) || Screen.hasAltDown())
-            && !this.stepcrafter$getMaintainingResources(this.getMenu().getRepository(), resource).isEmpty()
-            && this.stepcrafter$tryStartRequesting(resource)) {
-            return false;
+    @Inject(method = "mouseClicked(DDI)Z", at = @At("HEAD"), cancellable = true)
+    private void stepcrafter$mouseClicked(final double mouseX,
+                                          final double mouseY,
+                                          final int clickedButton,
+                                          final CallbackInfoReturnable<Boolean> cir) {
+        if (!Screen.hasAltDown()) {
+            return;
         }
-        return original && !Screen.hasAltDown(); // This is to not accidentally add another way to open the autocrafting preview screen
-    }
 
-    @ModifyReturnValue(method = "canExtract", at = @At("RETURN"))
-    private boolean canExtract(final boolean original) {
-        return original && !Screen.hasAltDown();
+        final GridResource resource = this.getCurrentGridResource();
+        if (resource == null) {
+            return;
+        }
+
+        if (!this.stepcrafter$getMaintainingResources(this.getMenu().getRepository(), resource).isEmpty()) {
+            this.stepcrafter$tryStartRequesting(resource);
+        }
+
+        cir.setReturnValue(true);
     }
 
     @Inject(method = "getAmountText", at = @At("HEAD"), remap = false, cancellable = true)
